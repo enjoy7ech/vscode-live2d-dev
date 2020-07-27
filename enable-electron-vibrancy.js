fe9d46c26ca4175d109c2b1ad53ba673d30872e5
zhenxiang <dongzx2016@gmail.com>
2020-07-27 17:28:12 +0800

    Update enable-electron-vibrancy.js

--- enable-electron-vibrancy.js
+++ enable-electron-vibrancy.js
@@ -1,5 +1,12 @@
 w = nodeRequire('electron').remote.getCurrentWindow();
-w.setBackgroundColor('#000000');
+w.setBackgroundColor('#00000000');
 // you should change the blur style if you like
 // light，ultra-light, dark, ultra-dark
-w.setVibrancy('light');
+w.setVibrancy('ultra-dark');
+const width = w.getBounds().width;
+w.setBounds({
+    width: width + 1,
+});
+w.setBounds({
+    width,
+});
